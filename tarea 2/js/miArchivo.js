b3279b11e6e01672ff4ec781727079b47e8602d8
Rodrigo <gwilkens.r@gmail.com>
2021-12-17 15:03:55 -0300

    tarea 2 lista

--- tarea 2/js/miArchivo.js
+++ tarea 2/js/miArchivo.js
@@ -5,16 +5,33 @@ const adivinanza = randomIntFromInterval(1, 20);
 //console.log(adivinanza);
 
 window.onload = function (){
-    //var response = document.getElementById("response");
+    var retry = document.getElementById("retry");
     var guide = document.getElementById("guide");
+    var response = document.getElementById("respose");
 };
 
+var intentos = 5;
+console.log(intentos);
 function checkNum(num){
     if(parseInt(num) > adivinanza ){
         guide.innerHTML = "El número es menor";
+        cuenta();
     } else if (parseInt(num) < adivinanza){
         guide.innerHTML = "El número es mayor";
+        cuenta();
     } else {
         guide.innerHTML = "correcto!";
+
+    }
+}
+function cuenta(){
+    intentos--;
+    console.log(intentos);
+    if (intentos === 0) {
+        response.classList.remove('showMe');
+        response.classList.add('hideMe');
+        retry.classList.remove('hideMe');
+        retry.classList.add('showMe');
+        guide.innerHTML = "Perdiste!";
     }
 }
